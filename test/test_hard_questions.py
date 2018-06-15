2e0fd2e7e6df74904b6bcc43943d7db87aa6aed6
ChrisBrown46 <christopher.r.brown@aggiemail.usu.edu>
2018-06-14 20:23:55 -0600

    Updated hard question to actually be hard.
    Signed-off-by: Chris <christopher.r.brown@aggiemail.usu.edu>

--- test/test_hard_questions.py
+++ test/test_hard_questions.py
@@ -1,19 +1,30 @@
 import unittest
 import random
+import math
 
-from src.hard_questions import encode_bit, decode_bit_sequence
+from src.hard_questions import encode_object, decode_bit_sequence
+from src.medium_questions import Node
 
 
 class HardQuestionsTests(unittest.TestCase):
 
     def test_error_correction(self):
         accuracy = 0.99         # 99% accuracy required to pass the test
-        noise_ratio = 50        # 1 out of 50 bits are flipped
-        number_of_inputs = 50   # 50 tests to run
+        noise_ratio = 10  # 1 out of 50 bits are flipped
 
-        random.seed()
-        input_values = [random.randint(0, 1) for _ in range(number_of_inputs)]
-        encoded_input = [encode_bit(bit) for bit in input_values]
+        input_values = [
+            'Hello',
+            Node(),
+            15,
+            0,
+            None,
+            [1, 1, 2, 3, 5, 8],
+            {'Hello': 15},
+            3.141592,
+            math.pi,
+            'World'
+        ]
+        encoded_input = [encode_object(bit) for bit in input_values]
 
         for i in range(len(encoded_input)):
             for j in range(len(encoded_input[i])):
@@ -24,11 +35,11 @@ class HardQuestionsTests(unittest.TestCase):
             decode_bit_sequence(sequence) for sequence in encoded_input
         ]
 
-        max_errors = (1.0 - accuracy) * number_of_inputs
+        max_errors = (1.0 - accuracy) * len(input_values)
         max_errors = int(max_errors)
-        actual_errors = int(number_of_inputs)
+        actual_errors = int(len(input_values))
 
-        for i in range(number_of_inputs):
+        for i in range(len(input_values)):
             if input_values[i] != output_values[i]:
                 actual_errors -= 1
 
